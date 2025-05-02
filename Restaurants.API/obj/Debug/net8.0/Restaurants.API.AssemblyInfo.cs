96847cacc40fef77c6d0b1e7257785500ea365f2
Shubham Kumar <2090944@cognizant.com>
2025-05-02 17:16:59 +0530

    Day_2 || Name Correction

--- Restaurants.API/obj/Debug/net8.0/Restaurants.API.AssemblyInfo.cs
+++ Restaurants.API/obj/Debug/net8.0/Restaurants.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurants.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bcefa7096a04087663d2df31535851a221b9c03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b484229d4cb838f6f672b08baa03a65d29b2d831")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurants.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurants.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Restaurants.Domain/obj/Debug/net8.0/Restaurants.Domain.AssemblyInfo.cs
+++ Restaurants.Domain/obj/Debug/net8.0/Restaurants.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurants.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bcefa7096a04087663d2df31535851a221b9c03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b484229d4cb838f6f672b08baa03a65d29b2d831")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurants.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurants.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
