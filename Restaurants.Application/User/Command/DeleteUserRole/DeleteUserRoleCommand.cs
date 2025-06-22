15cbd752fa01280abb1ec54dc95b73e41eb01427
Shubham Kumar <2090944@cognizant.com>
2025-06-22 18:04:52 +0530

    Day_14|| Delete User Role API (Missed Part)

---
+++ Restaurants.Application/User/Command/DeleteUserRole/DeleteUserRoleCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+namespace Restaurants.Application.User.Command.DeleteUserRole;
+public class DeleteUserRoleCommand:IRequest
+{
+    public string EmailId { get; set; }
+    public string RoleName { get; set; }
+
+}
---
+++ Restaurants.Application/User/Command/DeleteUserRole/DeleteUserRoleCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Restaurants.Domain.Exceptions;
+
+namespace Restaurants.Application.User.Command.DeleteUserRole;
+public class DeleteUserRoleCommandHandler (
+    UserManager<Domain.Entities.User>userManager,
+    RoleManager<IdentityRole>roleManager
+    ): IRequestHandler<DeleteUserRoleCommand>
+{
+    public async Task Handle(DeleteUserRoleCommand request, CancellationToken cancellationToken)
+    {
+        var user = await userManager.FindByEmailAsync(request.EmailId)
+            ?? throw new NotFoundException($"User Email {request.EmailId} not found in database;");
+        var role = await roleManager.FindByNameAsync(request.RoleName)
+            ?? throw new NotFoundException($"Role  :{request.RoleName} for Email: {request.EmailId} not found in database;");
+        await userManager.RemoveFromRoleAsync(user, role.Name!);
+    }
+}
