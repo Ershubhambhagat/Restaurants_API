83f6fc6dc7d33754878f1f14070c01f2f82fdf7b
Shubham Kumar <2090944@cognizant.com>
2025-07-12 00:41:51 +0530

    Day_18 || Try Catch in Program.cs

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -8,48 +8,49 @@ using Restaurants.Infrastructure.Seeders;
 using Serilog;
 using Serilog.Events;
 using Serilog.Formatting.Compact;
-
-//using Restaurants.Infrasturacture.Extensions1
-var builder = WebApplication.CreateBuilder(args);
-
-builder.AddPresentation();
-builder.Services.AddApplication();
-builder.Services.AddInfrastructure(builder.Configuration);
-
-
-
-
-
-
-
-var app = builder.Build();
-//This will construct RestaurantsSeeders before start app
-var scope = app.Services.CreateScope();
-var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantsSeeders>();
-await seeder.Seed();
-
-
-app.UseMiddleware<ErrorHandlingMiddleWare>();
-app.UseMiddleware<RequestTimeLoggingMiddleware>();
-app.UseSerilogRequestLogging();
-
-
-
-
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.AddPresentation();
+    builder.Services.AddApplication();
+    builder.Services.AddInfrastructure(builder.Configuration);
+
+    var app = builder.Build();
+    //This will construct RestaurantsSeeders before start app
+    var scope = app.Services.CreateScope();
+    var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantsSeeders>();
+    await seeder.Seed();
+
+    app.UseMiddleware<ErrorHandlingMiddleWare>();
+    app.UseMiddleware<RequestTimeLoggingMiddleware>();
+    app.UseSerilogRequestLogging();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.MapGroup("api/identity")
+        .WithTags("Identity")//this if for map two other endpoint to one
+        .MapIdentityApi<User>();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
 }
+catch (Exception ex)
+{
 
-app.UseHttpsRedirection();
-app.MapGroup("api/identity")
-    .WithTags("Identity")//this if for map two other endpoint to one
-    .MapIdentityApi<User>();
-app.UseAuthorization();
+    Log.Fatal(ex.Message, "Application Statup Faild ");
+}
+finally
+{
+    Log.CloseAndFlush();
 
-app.MapControllers();
-
-app.Run();
+}
